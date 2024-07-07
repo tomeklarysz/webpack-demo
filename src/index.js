857268131f6b05550cb079950f32f7890744ebd6
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-07 13:17:31 +0200

    es6 modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import _ from 'lodash';
+import myName from "./myName";
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = myName('Cody');
 
   return element;
 }
